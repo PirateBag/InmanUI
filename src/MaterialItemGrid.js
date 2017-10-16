4db44e7ea059ca0c00be6ffebd1c7e9a00de9f6c
osboxes.org <bagley.michael@gmail.com>
2017-10-16 16:42:43 -0400

    Checkin Du Jour.

--- src/MaterialItemGrid.js
+++ src/MaterialItemGrid.js
@@ -12,24 +12,69 @@ import PropTypes from 'prop-types';
 import NumberFormat from 'react-number-format';
 
 export class MaterialItemGrid extends Component {
-    constructor(props) {
-        super(props)
+    constructor () {
+        super();
+        this.state = ({
+            isSelected: []
+        });
 
+
+        this.onRowSelection = this.onRowSelection.bind(this);
+        this.isSelectedList = this.isSelectedList.bind(this);
     }
-    activeFormatter( cell, row ) {
+     activeFormatter( cell, row ) {
         return(
             <NumberFormat value={cell} thousandSeparator={','} decimalSeparator={'.'} prefix={'$'} decimalPrecision={2}
                           displayType='text'/>
         );
     }
 
+    onRowSelection(rows){
+
+        let  action = "insert";
+        let  newArray = [];
+        this.state.isSelected.map( ( selection ) => {
+            /*  Find a match, then remove from newArray to cause de-selection.  */
+            if ( rows[ 0 ] === selection ) {
+                action = "none";
+            } else {
+                /*  No match, then this entry's state is unchanged.  */
+                newArray.push( selection );
+            }
+        } );
+
+        /*  Never found any match, must be a newly selected item.  */
+        if ( action === "insert" ) {
+            newArray.push( rows[ 0 ]);
+        }
+
+        this.setState( {isSelected : newArray} );
+    }
+
+    isSelectedList( ) {
+        let response = [];
+        let i = 0;
+        for ( i = 0; i < this.props.ItemResponse.data.length; i ++ ) {
+            response[ i ] = false;
+            this.state.isSelected.forEach( ( selectionIndex ) => {
+                if ( selectionIndex === i ) {
+                    response[ i ] = true;
+                }
+            } );
+        }
+        return response;
+    }
+
     render() {
+
         if ( this.props.ItemResponse
          &&  this.props.ItemResponse.data ) {
             var arrayOfData = this.props.ItemResponse.data
             return (
                 <span>
-                     <Table>
+                    <p>Selected  {this.state.isSelected}</p>
+                    <p>isSelectList  {this.isSelectedList()}</p>
+                    <Table multiSelectable={true} displaySelectAll={false} onRowSelection={this.onRowSelection}>
                     <TableHeader>
                       <TableRow>
                         <TableHeaderColumn>ID</TableHeaderColumn>
@@ -41,15 +86,14 @@ export class MaterialItemGrid extends Component {
                     <TableBody stripedRows={true}>
                         { arrayOfData.map( (item ) => {
                             return(
-                                <TableRow key={item.id} >
-                                    <TableRowColumn style={{height: '12' }}>{item.id}</TableRowColumn>
-                                    <TableRowColumn style={{height: '12' }}>{item.summaryId}</TableRowColumn>
-                                    <TableRowColumn style={{height: '12' }}>{item.description}</TableRowColumn>
-                                    <TableRowColumn  style={{textAlign: 'right',height: '12'}}>
+                                <TableRow key={item.id} selected={true}>
+                                    <TableRowColumn>{item.id}</TableRowColumn>
+                                    <TableRowColumn>{item.summaryId}</TableRowColumn>
+                                    <TableRowColumn>{item.description}</TableRowColumn>
+                                    <TableRowColumn>
                                         <NumberFormat value={item.unitCost} thousandSeparator={','} decimalSeparator={'.'} prefix={'$'} decimalPrecision={2}
                                                   displayType='text'/>
                                     </TableRowColumn>
-
                                 </TableRow>
                             ); } ) }
                         }
